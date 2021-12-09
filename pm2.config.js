a89aa578b6578af1e772b66c4d7c3a799d314e61
Gareth Field <field.gareth@gmail.com>
2021-12-09 07:25:49 -0500

    added pm2 app for web

--- pm2.config.js
+++ pm2.config.js
@@ -1,20 +1,35 @@
-const name = 'huppup.app'; // Name to use in PM2
+const apiName = 'huppup.app-api'; // Name to use in PM2
+const webName = 'huppup.app-web'; // Name to use in PM2
 const repo = 'git@gitlab.com:twitchkidd/huppup.app.git'; // Link to your repo
 const user = 'gareth'; // Server user
-const path = `/home/${user}/${name}`; // Path on the server to deploy to
+const path = `/home/${user}/huppup.app`; // Path on the server to deploy to
 const host = 'localhost'; // Server hostname
-const port = 8911; // Port to use locally on the server
+const apiPort = 8911; // Port to use locally on the server
+const webPort = 8910; // Port to use locally on the server
 const build =
   'yarn install && yarn rw build && yarn rw prisma migrate deploy && yarn rw prisma db seed'; // Build commands
 
 module.exports = {
   apps: [
     {
-      name,
+      name: apiName,
       node_args: '-r dotenv/config',
       cwd: `${path}/current/`,
       script: 'yarn rw serve api',
-      args: `--port ${port}`,
+      args: `--port ${apiPort}`,
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: webName,
+      node_args: '-r dotenv/config',
+      cwd: `${path}/current/`,
+      script: 'yarn rw serve web',
+      args: `--port ${webPort}`,
       env: {
         NODE_ENV: 'development',
       },
